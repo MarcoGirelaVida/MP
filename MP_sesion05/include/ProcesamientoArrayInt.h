125d4e1b3655a221673d4e1333659ddf9918b494
Marco Girela Vida <marcogirelavida@gmail.com>
2023-03-20 13:36:29 +0100

    Función "MezclaVectoresSelectiva" añadida al .h

--- MP_sesion05/include/ProcesamientoArrayInt.h
+++ MP_sesion05/include/ProcesamientoArrayInt.h
@@ -99,14 +99,29 @@ void OrdenaIntercambioMejorado (int *v, int pos_inic, int pos_fin);
 
 void MezclaVectoresSimple (int *mezcla, int *v1, int util_v1, 
 	                                    int *v2, int util_v2);
-
 /***************************************************************************/
 /***************************************************************************/
-// Funciones de utilidad
+// Mezcla selectiva, guarda solo aquellos números que no se hayan repetido.
+// 
+// Parámetros:
+//		mezcla, dirección de memoria del inicio el vector resultado  
+//		v1, dirección de memoria del inicio del primer vector a mezclar  
+//		util_v1, número de casillas ocupadas en "v1"
+//		v2, dirección de memoria del inicio del segundo vector a mezclar  
+//		util_v2, número de casillas ocupadas en "v2"
+// PRE: "v1" y "v2" están ordenados
+// PRE: "mezcla" tiene suficiente memoria asignada
+// PRE: util_v1, util_v2 >= 0
+/*
+	EJEMPLO DE FUNCIONAMIENTO:
+	Si en v1 hay un 8 y en v2 también, no se mete 8 en "mezcla".
+	Si en v1 hay un 2 y en v2 no, se meterá un 2 en mezcla.
+	Además, si en v1 hay {..., 8, 8, 8, ...} y en v2 no hay ningún 8,
+	entonces solo se meterá UN 8 en "mezcla" (no los tres).
+*/
 
-// Intercambia los valores cuyas direcciones están en "p1" y "p2"
-
-void Intercambia (int *p1, int *p2);	
+int MezclaVectoresSelectiva (int mezcla[], int v1[],
+							int util_v1, int v2[], int util_v2);
 
 /***************************************************************************/
 /***************************************************************************/
@@ -128,5 +143,12 @@ void MezclaVectores (int mezcla[], int &util_mezcla,
 
 /***************************************************************************/
 /***************************************************************************/
+// Funciones de utilidad
 
+// Intercambia los valores cuyas direcciones están en "p1" y "p2"
+
+void Intercambia (int *p1, int *p2);
+
+/***************************************************************************/
+/***************************************************************************/
 #endif
